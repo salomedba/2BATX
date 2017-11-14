7cc99d720648d79df87ae467e02d37aecaa88dfc
salomedba <33451069+salomedba@users.noreply.github.com>
2017-11-14 11:36:30 +0100

    Add files via upload

---
+++ funcioavtivitat9_Salome.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+def rectangle (x, y):
+	for i in range(x):
+		for j in range (y):
+			print carac,
+		print " "
+	return
+	
+x = int(input("Altura del rectàngle: "))
+y = int(input("Amplaria del rectàngle: "))
+carac = raw_input("Quin caràcter vols imprimir? ") 
+rectangle (x, y)
